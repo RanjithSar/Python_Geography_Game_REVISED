5a76be58b3db82897daec30d94fc133a0602c208
Ranjith <itssrranjith@gmail.com>
2025-05-15 15:35:55 -0400

    added title banner

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tkinter as tk
+import tkinter.font as tkFont
 from CONSTANTS import *
 
 game_window = tk.Tk()
 
 game_window.minsize(WIDTH,HEIGHT)
+
+TITLE_FONT = tkFont.Font(family="Times New Roman",size=30)
+
+welcome_banner = tk.Label(
+    text="Test your U.S. geography skills here!",
+    font=TITLE_FONT
+)
+welcome_banner.pack()
 
 game_window.mainloop()
