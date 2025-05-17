24f5d45a7757f04a26ec6d988c2efb935b2c81c5
Ranjith <itssrranjith@gmail.com>
2025-05-17 15:40:52 -0400

    generate_question method now chooses four random city and one of those four is the correct answer to the question

--- question.py
+++ question.py
@@ -1,4 +1,5 @@
 from city_bank import City_Information
+import random
 
 class Question:
     
@@ -11,3 +12,18 @@ class Question:
         self.question_type = new_type
         
     def generate_question(self):
+        
+        cities = []
+        for i in range(4):
+            cities.append(Question.city_info.choose_random_city())
+            
+        print(cities)
+        print()
+            
+        correct_option_index = random.randint(0,3)
+        print(cities[correct_option_index])
+        
+        
+if __name__ == "__main__":
+    first_question = Question()
+    first_question.generate_question()
